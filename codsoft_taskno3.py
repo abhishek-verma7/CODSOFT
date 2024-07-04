9c8daf8cb592e6bcf6cb2818045cd9ef8b092da1
abhishek-verma7 <abhisv8953@gmail.com>
2024-07-04 10:18:38 +0530

    codsoft_taskno3.py

--- codsoft_taskno3.py
+++ codsoft_taskno3.py
@@ -3,28 +3,23 @@ from tkinter import messagebox
 import random
 import string
 
-# Function to generate password
 def generate_password():
     try:
-        length = int(length_entry.get())  # Get desired length of password
+        length = int(length_entry.get())  
         if length <= 0:
             messagebox.showerror("Error", "Password length must be greater than zero.")
             return
         
-        # Generate password
         password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
         
-        # Update the password display label
         password_var.set(password)
         
     except ValueError:
         messagebox.showerror("Error", "Please enter a valid integer for password length.")
 
-# Create main window
 root = tk.Tk()
 root.title("Password Generator")
 
-# Create and place widgets
 length_label = tk.Label(root, text="Password Length:")
 length_label.pack(pady=10)
 
@@ -41,5 +36,4 @@ password_label.pack(pady=10)
 password_display = tk.Label(root, textvariable=password_var, relief="solid", width=30)
 password_display.pack()
 
-# Run the main loop
 root.mainloop()
